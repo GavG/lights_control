ecc27ae3f3e8eb07b9628fac60ed84f3ece17170
GavG <gavingration@gmail.com>
2019-12-08 15:14:59 +0000

    twinkle

---
+++ twinkle.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+from random import randint
+import time import sleep
+
+GPIO.setmode(GPIO.BCM)
+
+pins = [17, 22, 23, 24,]
+
+for pin in pins:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.HIGH)
+
+while True:
+    sleep(randint(2, 10))
+    t_pin = pins[randint(0, 3)]
+    GPIO.output(t_pin, GPIO.LOW)
+    sleep(0.1)
+    GPIO.output(t_pin, GPIO.HIGH)
